0877310c9c3c4ac93265bb84a8a54f819521a4d8
Yuliamsyah <yuliamsyah1@gmail.com>
2022-12-07 05:57:28 +0700

    tabel lagi

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -31,21 +31,11 @@
     <link href="<?= base_url('/aset'); ?>/css/custom.min.css" rel="stylesheet">
 
     <!-- Datatables -->
-    <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-    <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-    <script src="../vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
-    <script src="../vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="../vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="../vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-    <script src="../vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-    <script src="../vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-    <script src="../vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
-    <script src="../vendors/jszip/dist/jszip.min.js"></script>
-    <script src="../vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
+    <link href="<?= base_url('/aset'); ?>/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('/aset'); ?>/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('/aset'); ?>/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('/aset'); ?>/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url('/aset'); ?>/css/scroller.bootstrap.min.css" rel="stylesheet">
 
 
 
